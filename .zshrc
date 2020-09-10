4aaaa8d16692a95a844dc4ee4ec741b37e1d7d59
root <root@pop-os.localdomain>
2020-09-11 01:36:43 +0200

    update

--- .zshrc
+++ .zshrc
@@ -2,13 +2,13 @@
 # export PATH=$HOME/bin:/usr/local/bin:$PATH
 
 # Path to your oh-my-zsh installation.
-export ZSH="/home/danaen/.oh-my-zsh"
+export ZSH="/root/.oh-my-zsh"
 
 # Set name of the theme to load --- if set to "random", it will
 # load a random theme each time oh-my-zsh is loaded, in which case,
 # to know which specific one was loaded, run: echo $RANDOM_THEME
 # See https://github.com/ohmyzsh/ohmyzsh/wiki/Themes
-ZSH_THEME="bira"
+ZSH_THEME="robbyrussell"
 
 # Set list of themes to pick from when loading at random
 # Setting this variable when ZSH_THEME=random will cause zsh to load
@@ -75,7 +75,6 @@ source $ZSH/oh-my-zsh.sh
 # User configuration
 
 # export MANPATH="/usr/local/man:$MANPATH"
-export PATH="$PATH:/snap/bin"
 
 # You may need to manually set your language environment
 # export LANG=en_US.UTF-8
@@ -98,5 +97,3 @@ export PATH="$PATH:/snap/bin"
 # Example aliases
 # alias zshconfig="mate ~/.zshrc"
 # alias ohmyzsh="mate ~/.oh-my-zsh"
-alias update="sudo apt update"
-alias upgrade="sudo apt update && sudo apt --assume-yes upgrade"
--- setup.sh
+++ setup.sh
@@ -1,64 +1,104 @@
+#EXTEND SUDO TIMEOUT
+echo "\nTRY TO EXTEND SUDO TIMEOUT\n(will most likely fail)\n"
+sudo echo "Defaults:${USER} timestamp_timeout=120" >> /etc/sudoers
+
 # BASICS
 sudo apt update
 sudo apt --assume-yes upgrade
 
 # INSTALL APTs
-echo "INSTALLING VIM"
+echo "\nINSTALLING VIM\n"
 sudo apt --assume-yes install vim
-echo "INSTALLING GIT"
+echo "\nINSTALLING GIT\n"
 sudo apt --assume-yes install git
-echo "INSTALLING FIRACODE FONTS"
+echo "\nINSTALLING FIRACODE FONTS\n"
 sudo apt --assume-yes install fonts-firacode
-echo "INSTALLING GNOME TWEAKS"
+echo "\nINSTALLING GNOME TWEAKS\n"
 sudo apt --assume-yes install gnome-tweaks
-echo "INSTALLING STACER"
+echo "\nINSTALLING STACER\n"
 sudo apt --assume-yes install stacer
-echo "INSTALLING DOCKER"
+echo "\nINSTALLING DOCKER\n"
 sudo apt --assume-yes install docker
-echo "INSTALLING CHROMIUM"
+echo "\nINSTALLING CHROMIUM\n"
 sudo apt --assume-yes install chromium
-echo "INSTALLING LIBRE OFFICE"
+echo "\nINSTALLING LIBRE OFFICE\n"
 sudo apt --assume-yes install libreoffice
-echo "INSTALLING SNAP"
+echo "\nINSTALLING SNAP\n"
 sudo apt --assume-yes install snapd
-echo "INSTALLING GOOGLE CHROME"
+echo "\nINSTALLING GOOGLE CHROME\n"
 sudo sh -c 'echo "deb [arch=amd64] https://dl.google.com/linux/chrome/deb/ stable main" > /etc/apt/sources.list.d/google-chrome.list'
 wget -q -O - https://dl-ssl.google.com/linux/linux_signing_key.pub | sudo apt-key add -
 sudo apt update
 sudo apt --assume-yes install google-chrome-stable
 
 # INSTALL SNAPs
+echo "\nINSTALLING SPOTIFY\n"
 sudo snap install spotify
+echo "\nINSTALLING POSTMAN\n"
 sudo snap install postman
+echo "\nINSTALLING SLACK\n"
 sudo snap install slack --classic
+echo "\nINSTALLING ROCKETCHAT\n"
 sudo snap install rocketchat-desktop
+echo "\nINSTALLING DISCORD\n"
 sudo snap install discord
+echo "\nINSTALLING GITTER\n"
 sudo snap install gitter-desktop
+echo "\nINSTALLING CAPRINE\n"
 sudo snap install caprine
+echo "\nINSTALLING OPERA\n"
 sudo snap install opera-developer
+echo "\nINSTALLING WHATSAPP\n"
 sudo snap install kesty-whatsapp
+echo "\nINSTALLING MAILSPRING\n"
 sudo snap install mailspring
+echo "\nINSTALLING VISUAL STUDIO CODE\n"
 sudo snap install code --classic
+echo "\nINSTALLING SUBLIME TEXT\n"
 sudo snap install sublime-text --classic
+echo "\nINSTALLING GIT KRAKEN\n"
 sudo snap install gitkraken --classic
+echo "\nINSTALLING NODE-RED\n"
 sudo snap install node-red
+echo "\nINSTALLING VLC\n"
 sudo snap install vlc
+echo "\nINSTALLING PYCHARM\n"
 sudo snap install pycharm-community --classic
+echo "\nINSTALLING ANDROID STUDIO\n"
 sudo snap install android-studio --classic
+echo "\nINSTALLING INTELLIJ IDEA\n"
 sudo snap install intellij-idea-community --classic
+echo "\nINSTALLING PHPSTORM\n"
 sudo snap install phpstorm --classic
+echo "\nINSTALLING ZENKIT\n"
 sudo snap install zenkit
+echo "\nINSTALLING FROMSCRATCH\n"
 sudo snap install fromscratch
+echo "\nINSTALLING KATA CONTAINERS\n"
 sudo snap install kata-containers --classic
+echo "\nINSTALLING DRAWIO\n"
 sudo snap install drawio
+echo "\nINSTALLING RAMBOX\n"
 sudo snap install rambox
 
-echo "INSTALLING ZSH"
-sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+# CONFIGURE ENVIRONNEMENT
+echo "INSTALLING ZSH & OH MY ZSH"
+sudo apt --assume-yes install zsh
+sudo sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 chsh -s $(which zsh)
+
+cp ./.zshrc ${HOME}/.zshrc
 
 echo "CHANGE TIME SYSTEM"
 timedatectl set-local-rtc 1 --adjust-system-clock
 
 echo "SET WALLPAPER"
 gsettings set org.gnome.desktop.background picture-uri file://${PWD}/wallpapers/Our_moon_in_HDR.png
+
+echo "SET HOTKEYS"
+#Grab a window: screen gnome-screenshot -w
+#Grab an area: screen gnome-screenshot -a
+#Send the grab to the clipboard: gnome-screenshot -c
+
+# REBOOT SYSTEM
+sudo reboot
